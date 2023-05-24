c3d493dbd9b264bc0de28f3d0af4173ffd7d0bbe
我不写名字 <362409100@qq.com>
2023-05-24 12:06:43 +0800

    修改一下项目属性目录

--- SipService/pch.cpp
+++ SipService/pch.cpp
@@ -3,3 +3,26 @@
 #include "pch.h"
 
 // 当使用预编译的头时，需要使用此源文件，编译才能成功。
+
+std::string GenerateRandomString(int n)
+{
+	std::random_device rd;  
+	std::mt19937 gen(rd()); 
+	std::uniform_int_distribution<> dis(1, 1000);
+
+	const std::string chars("0123456789"
+		"abcdefghijklmnopqrstuvwxyz"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	int i = 0;
+	int len = (int)chars.size();
+	std::string text;
+	while (i < n)
+	{
+		int idx = dis(gen) % len;
+		text.push_back(chars[idx]);
+		++i;
+	}
+
+	return text;
+}
+
