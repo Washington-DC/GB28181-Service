4137b3e74636a76ffb7b16460db8ee77033ce645
我不写名字 <362409100@qq.com>
2023-06-26 11:06:10 +0800

    添加dll和exe

--- SipService/SipServer.h
+++ SipService/SipServer.h
@@ -23,7 +23,7 @@ private:
 	eXosip_t* _sip_context = nullptr;
 	std::string _sip_id = "34020000002000000001";
 	int _sip_port = 5060;
-	std::string _user_agent = "cdtye_gb28181";
+	std::string _user_agent = "yszs_gb28181";
 	uint64_t _event_id = 0;      // 事件id 自增
 	std::shared_ptr<std::thread> _event_thread = nullptr;
 
